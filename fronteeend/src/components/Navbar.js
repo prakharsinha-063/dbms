ab59ad94d83b42426f6438564c5c8f381ab1059e
harshpatel <harshpatel97762@gmail.com>
2025-05-29 02:05:03 +0530

    final code

--- fronteeend/src/components/Navbar.js
+++ fronteeend/src/components/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
             </>
           ) : (
             <>
-              <Link to="/">Home</Link>
+              
               <Link to="/login">Login</Link>
               <Link to="/register">Register</Link>
             </>
